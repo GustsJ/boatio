4ae53457b1ad11bccf5f64711dbdef03517b9618
gustsj/dev <gusts.jermacans@gig.com>
2022-02-26 21:24:48 +0200

    push

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from "./pages";
 
 function App() {
   return (
-    <Router>
-        <Routes>
-            <Route exact path="/" element={<Home/>}/>
-        </Routes>
-    </Router>
+      <Home/>
   );
 }
 
